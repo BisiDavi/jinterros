58909d3012ade1e4ced00101e98be9e28fe473d0
BisiDavi <oludavidconnect@gmail.com>
2022-09-02 12:11:07 +0100

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
   return (
     <>
       <footer className="bg-carton-brown footer w-full">
-        <div className="layer-1 w-full h-96 py-8">
-          <div className="container mx-auto">
+        <div className="layer-1 w-full py-20">
+          <div className="container mx-auto flex justify-between">
             <div className="w-1/2 left-pane">
               <div className="links-group grid grid-cols-3">
                 {links.footer.map((footerItem) => (
@@ -18,13 +18,51 @@ export default function Footer() {
                     {footerItem.group.map((groupItem) => (
                       <li key={groupItem.link} className="my-2">
                         <Link href={groupItem.link}>
-                          <a>{groupItem.text}</a>
+                          <a className="font-light">{groupItem.text}</a>
                         </Link>
                       </li>
                     ))}
                   </ul>
                 ))}
               </div>
+              <div className="bottom-content mt-32">
+                <p className="mb-4 text-xs">Copyright 2022 | Jinterros</p>
+                <ul className="flex items-center">
+                  {links.others.map((linkItem, index) => {
+                    const showBorder =
+                      Number(links.others.length - 1) !== index ? "|" : "";
+                    return (
+                      <li key={linkItem.link}>
+                        <Link href={linkItem.link}>
+                          <>
+                            <a className="underline hover:text-red-500 text-xs">
+                              {linkItem.text}
+                            </a>{" "}
+                            <span className="mr-1">{showBorder}</span>
+                          </>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+            <div className="w-1/2 right-pane items-right">
+              <h5 className="text-lg font-medium mb-3">FIND US</h5>
+              <ul className="flex">
+                {links.socials.map((social, index) => {
+                  const showBorder =
+                    Number(links.socials.length - 1) !== index ? "|" : "";
+                  return (
+                    <li key={social.text}>
+                      <a className="font-light">
+                        {social.text}
+                        <span className="mx-2">{showBorder}</span>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
         </div>
