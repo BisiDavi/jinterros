282ce44af8aa86937e4b0c1fbb55eff995e16f92
BisiDavi <oludavidconnect@gmail.com>
2022-09-07 19:24:21 +0100

    aos added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
 import type { AppProps } from "next/app";
 import Providerlayout from "@/layout/ProviderLayout";
 
+import "aos/dist/aos.css";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
   return (
     <Providerlayout>
       <Component {...pageProps} />
