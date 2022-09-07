3b6ee525359c7a7ed4c03ea3cf869fffa742812c
BisiDavi <oludavidconnect@gmail.com>
2022-09-07 18:03:21 +0100

    our story page updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="bg-white w-full shadow">
       <div className="container mx-auto flex h-20 items-center justify-between relative">
         <HeaderLinks section="left" />
-        <div className="w-52 mt-36">
+        <div className="w-52 mt-36 z-50">
           <Logo />
         </div>
         <HeaderLinks section="right" />
--- pages/our-story.tsx
+++ pages/our-story.tsx
@@ -2,31 +2,50 @@ import Image from "next/image";
 
 import DefaultLayout from "@/layout/DefaultLayout";
 import storyContent from "@/json/our-story.json";
+import Button from "@/components/button";
 
 export default function OurStory() {
   return (
     <DefaultLayout title="Our Story">
-      <>
+      <div className="content">
         <Image
           src="/our-story-banner.webp"
           alt="Our Story"
           height={1000}
           width={1500}
           layout="responsive"
+          className="mt-20"
         />
         <section className="content w-full">
           <div className="layer w-full">
-            <div className="text-content w-2/3 mx-auto">
+            <div className="text-content w-2/3 mx-auto my-26">
               {storyContent.map((item) => (
-                <div key={item.title} className="text my-6">
-                  <h4>{item.title}</h4>
-                  <p>{item.text}</p>
+                <div key={item.title} className="text my-10">
+                  <h4 className="text-tan font-semibold text-xl">
+                    {item.title}
+                  </h4>
+                  <p className="text-light-tan font-light text-lg">
+                    {item.text}
+                  </p>
                 </div>
               ))}
             </div>
           </div>
         </section>
-      </>
+        <div className="image-wrapper w-2/5 mx-auto my-40">
+          <Image
+            src="/rum-bottles-2.webp"
+            alt="Rum bottles"
+            height={1000}
+            width={1000}
+            layout="responsive"
+          />
+        </div>
+        <Button
+          text="SHOP NOW"
+          className="bg-dark-brown hover:opacity-80 mx-auto py-3 px-16 flex my-10 text-white font-bold text-xl"
+        />
+      </div>
     </DefaultLayout>
   );
 }
