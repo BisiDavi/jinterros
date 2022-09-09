7c2f6df69f8737b3eeae6c151b81fbad2cdd646f
BisiDavi <oludavidconnect@gmail.com>
2022-09-09 17:42:39 +0100

    cart updated

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -5,8 +5,8 @@ import DefaultLayout from "@/layout/DefaultLayout";
 export default function Cart() {
   return (
     <DefaultLayout title="Cart">
-      <section className="cart container w-4/5 mx-auto my-52">
-        <div className="w-3/4 shadow">
+      <section className="cart container w-4/5 mx-auto flex my-52">
+        <div className="w-4/5 shadow-lg">
           <h4 className="text-xl py-1 ml-4">Cart(1)</h4>
           <hr />
           <div className="view  w-4/5 flex mx-auto my-6 items-center justify-center ">
@@ -22,19 +22,35 @@ export default function Cart() {
               <h6 className="price text-xl font-bold text-center">$40</h6>
               <div className="row flex items-center justify-between mt-6">
                 <Button
-                  className="bg-rum-light-brown h-6 flex text-white items-center justify-center text-4xl w-10"
+                  className="bg-rum-light-brown h-6 flex text-white items-center justify-center text-4xl w-10 hover:opacity-80"
                   text="-"
                 />{" "}
                 <span className="font-bold text-2xl">1</span>{" "}
                 <Button
-                  className="bg-rum-brown h-6 flex items-center text-white justify-center text-3xl w-10"
+                  className="bg-rum-brown h-6 flex items-center text-white justify-center text-3xl w-10 hover:opacity-80"
                   text="+"
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className="w-1/4 shadow"></div>
+        <div className="w-2/5 shadow-lg ml-6">
+          <h4 className="text-xl py-1 ml-4">CART SUMMARY</h4>
+          <hr />
+          <div className="text flex items-center justify-between items-start my-20 px-4">
+            <div className="column">
+              <h3 className="text-2xl font-bold">Subtotal</h3>
+              <p className="text-sm">Delivery fees not included yet.</p>
+            </div>
+            <p className="text-xl font-bold">$40</p>
+          </div>
+          <hr />
+
+          <Button
+            text="CHECKOUT"
+            className="bg-rum-brown w-3/4 mx-auto my-10  flex items-center justify-center font-bold hover:opacity-80 h-10 text-white"
+          />
+        </div>
       </section>
     </DefaultLayout>
   );
