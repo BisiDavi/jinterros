ae91c2af1bc4f3ae32e1970cc6f225d94ad7bd06
BisiDavi <oludavidconnect@gmail.com>
2022-09-09 21:02:26 +0100

    home page updated

--- views/LearnmoreView.tsx
+++ views/LearnmoreView.tsx
@@ -1,22 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import Button from "@/components/button";
 
 export default function LearnmoreView() {
   return (
     <>
-      <section className="learnmore w-full flex items-center justify-center">
-        <div className="text-content py-44">
-          <h1 className="text-center text-5xl font-bold">JINTERROS</h1>
-          <hr className="border w-56 bg-brown-light mb-4 mx-auto" />
-          <p className="text text-xl text-center  mx-auto">
-            Jinterros is a family run business.
-            <br /> We worked together to create this magnificent drink so
-            everyone across the globe can enjoy it with their loved ones.
-          </p>
-          <Button
-            text="LEARN MORE"
-            className="border border-brown-light bg-dark-brown-hover hover:text-white px-20 py-4 flex mx-auto my-24"
-          />
-        </div>
+      <section className="learnmore w-full flex flex-col py-20 items-center justify-center">
+        <img
+          src="/JINTERROS.webp"
+          className="mx-auto mt-40"
+          alt="JINTERROS"
+          title="JINTERROS"
+        />
+        <p className="text text-xl text-center w-2/3  mx-auto">
+          Jinterros is a family run business.
+          <br /> We worked together to create this magnificent drink so everyone
+          across the globe can enjoy it with their loved ones.
+        </p>
+        <Button
+          text="LEARN MORE"
+          className="border border-brown-light w-1/3 h-20 items-center justify-center text-xl bg-dark-brown-hover hover:text-white px-20 py-4 flex mx-auto my-24"
+        />
       </section>
       <style jsx>
         {`
