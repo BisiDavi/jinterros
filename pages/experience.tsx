4d422ec0f1b4fcc8ea789a9afa4a6279d93609b8
BisiDavi <oludavidconnect@gmail.com>
2022-09-15 20:43:46 +0100

    experience page updated

---
+++ pages/experience.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "@/layout/DefaultLayout";
+
+export default function experience() {
+  return (
+    <DefaultLayout title="Our Experience">
+      <section>
+        
+      </section>
+    </DefaultLayout>
+  );
+}
