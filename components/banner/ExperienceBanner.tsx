c29249dfb38dd0cc9900b074e9a0e61fec9c29d4
BisiDavi <oludavidconnect@gmail.com>
2022-09-15 21:11:13 +0100

    experience page updated

--- components/banner/ExperienceBanner.tsx
+++ components/banner/ExperienceBanner.tsx
@@ -1,8 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
+import experienceContent from "@/json/experience.json";
+
 export default function ExperienceBanner() {
   return (
     <section className="experience-banner">
-      <img src="/experience-banner.webp" alt="experience" />
+      <img src="/bottles-rum-flower.webp" alt="experience" />
+      <ul className="text-content my-16 w-2/3 flex flex-col justify-center mx-auto">
+        {experienceContent.map((item) => (
+          <li key={item.title} className="my-4">
+            <h4 className="font-bold text-xl text-tan">{item.title}</h4>
+            <p className="text-dark-brown font-light">{item.text}</p>
+            {item.text1 && (
+              <p className="mt-4 text-dark-brown font-light">{item.text1}</p>
+            )}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
