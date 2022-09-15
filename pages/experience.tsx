4338b6759e5d112565ee7e29274fe9ad6ad7fefd
BisiDavi <oludavidconnect@gmail.com>
2022-09-15 22:53:26 +0100

    folder updated

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,5 +1,5 @@
 import ExperienceBanner from "@/components/banner/ExperienceBanner";
-import ExperienceSlider from "@/components/ddrdslider/ExperienceSlider";
+import ExperienceSlider from "@/components/slider/ExperienceSlider";
 import DefaultLayout from "@/layout/DefaultLayout";
 
 export default function experience() {
