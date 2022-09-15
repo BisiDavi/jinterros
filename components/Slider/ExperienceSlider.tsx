f58f7d594e947a3cb6f0ee8034a9a16371b17d66
BisiDavi <oludavidconnect@gmail.com>
2022-09-15 22:08:15 +0100

    images added

---
+++ components/Slider/ExperienceSlider.tsx
@@ -0,0 +1,44 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+const slider = {
+  big: [
+    "/01.webp",
+    "/02.webp",
+    "/03.webp",
+    "/04.webp",
+    "/05.webp",
+    "/06.webp",
+    "/07.webp",
+    "/08.webp",
+    "/09.webp",
+    "/10.webp",
+  ],
+  small: [
+    "/small-01.webp",
+    "/small-02.webp",
+    "/small-03.webp",
+    "/small-04.webp",
+    "/small-05.webp",
+    "/small-06.webp",
+    "/small-07.webp",
+    "/small-08.webp",
+    "/small-09.webp",
+    "/small-10.webp",
+  ],
+};
+
+export default function ExperienceSlider() {
+  const mobileWidth = useMediaQuery("(max-width:768px)");
+  return (
+    <Splide options={{ rewind: true }} aria-label="React Splide Example">
+      <SplideSlide>
+        <img src="image1.jpg" alt="Image 1" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src="image2.jpg" alt="Image 2" />
+      </SplideSlide>
+    </Splide>
+  );
+}
