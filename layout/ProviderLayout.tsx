145787919c4a5b0bcb8e2633e8b8a17b4c9b9c0b
BisiDavi <oludavidconnect@gmail.com>
2022-09-21 01:36:25 +0100

    tanstack configuration

--- layout/ProviderLayout.tsx
+++ layout/ProviderLayout.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { PropsWithChildren } from "react";
 import { ToastContainer } from "react-toastify";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import store from "@/redux/store";
 import NextNProgress from "@/components/loader/NextNProgress";
@@ -13,13 +15,29 @@ import BeerLoader from "@/components/loader/BeerLoader";
 export default function Providerlayout({ children }: PropsWithChildren<{}>) {
   let persistor = persistStore(store);
 
+  const queryClient: any = new QueryClient({
+    defaultOptions: {
+      queries: {
+        cacheTime: 1000 * 60 * 60 * 24,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        retry: false,
+        staleTime: 1000 * 60 * 30,
+      },
+    },
+  });
+
   return (
     <>
       <Provider store={store}>
         <PersistGate loading={<BeerLoader />} persistor={persistor}>
           <NextNProgress color="#4e44c4" />
-          <ToastContainer />
-          {children}
+          <QueryClientProvider client={queryClient} contextSharing={true}>
+            <ToastContainer />
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </PersistGate>
       </Provider>
     </>
