0b825137d66d4d2a2c7b02a0e47886c3ba2f30c3
BisiDavi <oludavidconnect@gmail.com>
2022-09-22 02:35:43 +0100

    phonenumber is required

--- components/form/ShippingForm.tsx
+++ components/form/ShippingForm.tsx
@@ -4,22 +4,33 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import checkoutForm from "@/json/checkout-form.json";
 import displayForm from "@/components/form/displayForm";
 import Button from "@/components/button";
+import { shippingformSchema } from "@/components/form/schema/shippingformSchema";
 
 export default function ShippingForm() {
   const methods = useForm({
-    // resolver: yupResolver(signinSchema),
+    resolver: yupResolver(shippingformSchema),
     mode: "all",
   });
+
+  function onSubmit(data: any) {
+    console.log("data", data);
+  }
 
   return (
     <div className="lg:w-3/5 order-2 lg:order-1  shadow-lg pb-8">
       <h4 className="text-xl py-1 ml-4 pt-3 h-12">SHIPPING ADDRESS</h4>
       <hr />
       <FormProvider {...methods}>
-        <form className="px-6 lg:px-10">
+        <form
+          className="px-6 lg:px-10"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
           {checkoutForm.map((formItem, index) => {
             return (
-              <div key={index} className="flex justify-between items-center">
+              <div
+                key={index}
+                className="flex mt-4 justify-between items-center"
+              >
                 {formItem.map((item, idx) => {
                   const firstClassName =
                     idx === 0 && formItem.length === 2 ? "mr-4" : "";
@@ -28,7 +39,10 @@ export default function ShippingForm() {
                       ? `lg:w-80 ${firstClassName}`
                       : "w-full";
                   return (
-                    <div key={item.name} className={`form-control my-3 ${width}`}>
+                    <div
+                      key={item.name}
+                      className={`form-control my-2 ${width}`}
+                    >
                       {displayForm(item)}
                     </div>
                   );
@@ -39,6 +53,7 @@ export default function ShippingForm() {
           <Button
             className="bg-rum-brown w-full flex items-center py-2 justify-center text-white font-bold mt-4"
             text="SAVE ADDRESS"
+            type="submit"
           />
         </form>
       </FormProvider>
--- components/form/schema/shippingformSchema.ts
+++ components/form/schema/shippingformSchema.ts
@@ -1,5 +1,24 @@
 import * as yup from "yup";
 
 export const shippingformSchema = yup.object({
-    
-})
+  firstName: yup
+    .string()
+    .min(3, "minium of three letters")
+    .matches(/^[a-zA-Z_ ]*$/, "Only letters")
+    .required("your first name is required"),
+  lastName: yup
+    .string()
+    .min(3, "minium of three letters")
+    .matches(/^[a-zA-Z_ ]*$/, "Only letters")
+    .required("your last name is required"),
+  address1: yup.string().required("Address 1 is required"),
+  phoneNumber: yup.string().required("Phone number is required"),
+  email: yup
+    .string()
+    .email("Enter a vaild email address")
+    .required("E-mail address is required"),
+  city: yup.string().required("City is required"),
+  state: yup.string().required("State is required"),
+  zip: yup.string().required("Zip is required"),
+  country: yup.string().required("Country is required"),
+});
