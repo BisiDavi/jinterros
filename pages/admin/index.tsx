9441e70afe2d8839312987ff639154c2e3ea83fa
BisiDavi <oludavidconnect@gmail.com>
2022-09-23 16:13:07 +0100

    admin page updated

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,4 @@
+import type { GetServerSidePropsContext } from "next";
 import AdminLayout from "@/layout/AdminLayout";
 import AdminOrderSummary from "@/views/AdminOrderSummary";
 import InfoCardGroupView from "@/views/InfoCardGroupView";
@@ -9,4 +10,21 @@ export default function Admin() {
       <AdminOrderSummary />
     </AdminLayout>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+
+  if (!req.cookies?.admin) {
+    return {
+      redirect: {
+        destination: "/admin/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
