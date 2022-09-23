afbaf9239e11b60a3e3ab134a6f9ea90d3c1a35a
BisiDavi <oludavidconnect@gmail.com>
2022-09-23 16:14:33 +0100

    admin page updated

--- pages/admin/cocktails.tsx
+++ pages/admin/cocktails.tsx
@@ -1,5 +1,23 @@
 import AdminLayout from "@/layout/AdminLayout";
+import { GetServerSidePropsContext } from "next";
 
 export default function AdminCocktailsPage() {
   return <AdminLayout title="Cocktails"></AdminLayout>;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+
+  if (!req.cookies?.admin) {
+    return {
+      redirect: {
+        destination: "/admin/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
--- pages/admin/products.tsx
+++ pages/admin/products.tsx
@@ -1,5 +1,23 @@
+import type { GetServerSidePropsContext } from "next";
 import AdminLayout from "@/layout/AdminLayout";
 
 export default function Products() {
   return <AdminLayout title="Products"></AdminLayout>;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+
+  if (!req.cookies?.admin) {
+    return {
+      redirect: {
+        destination: "/admin/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
