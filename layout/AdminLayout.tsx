e626b30e71d06489139e092fff2745dddccc04bc
BisiDavi <oludavidconnect@gmail.com>
2022-09-23 15:31:24 +0100

    admin links updated

--- layout/AdminLayout.tsx
+++ layout/AdminLayout.tsx
@@ -39,7 +39,7 @@ export default function AdminLayout({
               return (
                 <li
                   key={item.link}
-                  className={`${activeLink} my-4 pl-8 flex items-center hover:bg-gray-300 p-2 text-brown-old-hover`}
+                  className={`${activeLink} pl-8 flex items-center hover:bg-gray-300 p-2 text-brown-old-hover`}
                 >
                   {displayIcons(item.text)}
                   <Link href={item.link} passHref>
--- lib/displayIcons.tsx
+++ lib/displayIcons.tsx
@@ -1,6 +1,9 @@
 import { GiWineBottle } from "react-icons/gi";
 import { FaClipboardList } from "react-icons/fa";
 import { BsCurrencyDollar } from "react-icons/bs";
+import { MdOutlinePolicy } from "react-icons/md";
+import { FaCocktail } from "react-icons/fa";
+import { MdOutlinePages } from "react-icons/md";
 
 import DashboardIcon from "@/public/icons/DashboardIcon";
 import OrdersIcon from "@/public/icons/OrdersIcon";
@@ -13,12 +16,20 @@ export default function displayIcons(icon: string) {
     case "Products":
       return <GiWineBottle size={50} />;
     case "Orders":
-      return <FaClipboardList size={47} />;
+      return <FaClipboardList size={40} />;
     case "TOTAL REVENUE":
       return <BsCurrencyDollar size={40} color="white" />;
     case "TOTAL ORDERS":
       return <OrdersIcon color="white" />;
     case "TOTAL CUSTOMERS":
       return <PeopleIcon />;
+    case "Policies":
+      return <MdOutlinePolicy size={40} />;
+    case "Cocktails":
+      return <FaCocktail size={40} />;
+    case "Pages":
+      return <MdOutlinePages size={40} />;
   }
 }
+
+// MdOutlinePages
