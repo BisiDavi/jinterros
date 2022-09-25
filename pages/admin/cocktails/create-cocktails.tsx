5eb927e67b007ff079308a2c01236981aa2ee468
BisiDavi <oludavidconnect@gmail.com>
2022-09-25 04:11:39 +0100

    cocktails pages added

---
+++ pages/admin/cocktails/create-cocktails.tsx
@@ -0,0 +1,28 @@
+import CocktailForm from "@/components/form/CocktailForm";
+import AdminLayout from "@/layout/AdminLayout";
+import { GetServerSidePropsContext } from "next";
+
+export default function AdminCocktailsPage() {
+  return (
+    <AdminLayout title="Cocktails">
+      <CocktailForm />
+    </AdminLayout>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+
+  if (!req.cookies?.admin) {
+    return {
+      redirect: {
+        destination: "/admin/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
