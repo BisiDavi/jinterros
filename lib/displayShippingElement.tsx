489e6f940ebec332fabac418d920066dafa17def
BisiDavi <oludavidconnect@gmail.com>
2022-09-26 18:32:35 +0100

    input form updated

--- lib/displayShippingElement.tsx
+++ lib/displayShippingElement.tsx
@@ -10,7 +10,7 @@ export default function displayShippingElement(
       {formItem.map((item, idx) => {
         const firstClassName = idx === 0 && formItem.length === 2 ? "mr-4" : "";
         const width =
-          formItem.length === 2 ? `lg:w-80 ${firstClassName}` : "w-full";
+          formItem.length === 2 ? `w-1/2 ${firstClassName}` : "w-full";
         const elementKey = `${item.name}-${idx}`;
         return (
           <div key={elementKey} className={`form-control my-2 ${width}`}>
