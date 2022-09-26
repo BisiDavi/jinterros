22406072211dc03bc08015acf5243a193eccd727
BisiDavi <oludavidconnect@gmail.com>
2022-09-26 18:49:51 +0100

    shop updated

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
 import { useRouter } from "next/router";
 
 import DefaultLayout from "@/layout/DefaultLayout";
@@ -23,27 +22,20 @@ export default function ShopPage() {
           “A party is best enjoyed with a circle of friends and a bottle of
           rum.”
         </h4>
-        <div className="image-wrapper -mb-24 lg:hidden mx-auto w-2/5 lg:w-auto justify-center mt0">
-          <Image
-            src="/rum-bottle.webp"
-            alt="jinterros"
-            title="jinterros"
-            height={800}
-            width={300}
-            layout="responsive"
-          />
-        </div>
+        <img
+          src="/rum-bottle.webp"
+          alt="jinterros"
+          title="jinterros"
+          className="image-wrapper -mb-4 lg:hidden mx-auto w-2/5 lg:w-auto justify-center mt0"
+        />
         <div className="shop-view bg-orange flex flex-col lg:flex-row px-8 py-32  lg:p-20 lg-h-800  mb-40 shop-background-image w-screen">
-          <div className="w-full hidden lg:flex lg:w-1/2 relative">
-            <div className="image-wrapper mx-auto flex justify-center mt0">
-              <Image
-                src="/rum-bottle.webp"
-                alt="jinterros"
-                title="jinterros"
-                height={800}
-                width={300}
-              />
-            </div>
+          <div className="w-full hidden h-full lg:flex lg:w-1/2 relative">
+            <img
+              src="/rum-bottle.webp"
+              alt="jinterros"
+              title="jinterros"
+              className="image-wrapper mx-auto w-1/2 h-full flex justify-center mt-40"
+            />
           </div>
           <div className="w-full lg:w-1/2 lg:text-xl font-bold">
             <h6 className="text-white lg:mb-10 lg:w-3/4 lg:mt-20 ">
