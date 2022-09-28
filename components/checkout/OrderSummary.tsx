cb5be59b3f48be34a1e706d578608e4234d816ee
BisiDavi <oludavidconnect@gmail.com>
2022-09-29 00:53:24 +0100

    order summary updated

--- components/checkout/OrderSummary.tsx
+++ components/checkout/OrderSummary.tsx
@@ -6,9 +6,9 @@ import useCart from "@/hooks/useCart";
 export default function OrderSummary() {
   const { cart, deliveryFee, getSubtotal } = useCart();
 
-  const subtotal = getSubtotal()
+  const subtotal = getSubtotal();
 
-  const total = subtotal + deliveryFee
+  const total = subtotal + deliveryFee;
 
   return (
     <div className="w-full order-1 lg:order-2 lg:w-2/5 shadow-lg mb-10 lg:mb-0 lg:ml-10">
@@ -20,24 +20,26 @@ export default function OrderSummary() {
             cart.map((item) => (
               <div
                 key={item.title}
-                className="row flex lg:items-center w-1/2 lg:w-3/4 justify-between mx-auto"
+                className="row flex lg:items-center border-b  w-1/2 lg:w-full px-4 justify-between mx-auto"
               >
-                <img
-                  src={item.img}
-                  alt={item.title}
-                  title={item.title}
-                  className="w-1/3 lg:w-auto"
-                />
-                <div className="text flex flex-col text-xl">
-                  <h6 className="text-md lg:text-2xl font-thin">
-                    {item.quantity} {item.quantity > 1 ? "Bottles" : "Bottle"}{" "}
-                    of Rum
-                  </h6>
-                  <h4 className="text-rum-brown font-bold">${item.price}</h4>
-                  <p className="text-base">
-                    Qty:
-                    <span className="font-bold ml-1">{item.quantity}</span>
-                  </p>
+                <div className="content w-2/3 mb-2 flex items-center justify-center mx-auto">
+                  <img
+                    src={item.img}
+                    alt={item.title}
+                    title={item.title}
+                    className="w-1/3 lg:w-1/5"
+                  />
+                  <div className="text lg:w-3/4 ml-4 flex flex-col text-xl">
+                    <h6 className="text-md lg:text-2xl font-thin">
+                      {item.quantity} {item.quantity > 1 ? "Bottles" : "Bottle"}{" "}
+                      of Rum
+                    </h6>
+                    <h4 className="text-rum-brown font-bold">${item.price}</h4>
+                    <p className="text-base">
+                      Qty:
+                      <span className="font-bold ml-1">{item.quantity}</span>
+                    </p>
+                  </div>
                 </div>
               </div>
             ))}
