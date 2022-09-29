4ee22dc6f27b10f37889dea108b6d8cbfd063c75
BisiDavi <oludavidconnect@gmail.com>
2022-09-29 13:51:16 +0100

    paypal updated

--- components/payment/Paypal.tsx
+++ components/payment/Paypal.tsx
@@ -19,36 +19,41 @@ export default function Paypal() {
   } = useAppSelector((state) => state.form);
 
   return (
-    <PayPalScriptProvider
-      options={{
-        "client-id": `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`,
-        currency: "USD",
-        components: "buttons",
-        intent: "capture",
-      }}
-    >
-      <PayPalButtons
-        className="mx-auto flex justify-center mb-8 w-1/3"
-        disabled={!completed}
-        style={{ layout: "vertical", label: "checkout" }}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: `${total}`,
+    <div className="wrapper bg-white relative z-10">
+      <PayPalScriptProvider
+        options={{
+          "client-id": `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`,
+          currency: "USD",
+          components: "buttons",
+          intent: "capture",
+        }}
+      >
+        <PayPalButtons
+          className="mx-auto flex justify-center mb-8 w-1/3"
+          disabled={!completed}
+          style={{
+            layout: "vertical",
+            label: "checkout",
+          }}
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: `${total}`,
+                  },
                 },
-              },
-            ],
-          });
-        }}
-        onApprove={(data, actions: any) => {
-          return actions.order.capture().then((details: any) => {
-            const name = details.payer.name.given_name;
-            console.log("details", details);
-          });
-        }}
-      />
-    </PayPalScriptProvider>
+              ],
+            });
+          }}
+          onApprove={(data, actions: any) => {
+            return actions.order.capture().then((details: any) => {
+              const name = details.payer.name.given_name;
+              console.log("details", details);
+            });
+          }}
+        />
+      </PayPalScriptProvider>
+    </div>
   );
 }
