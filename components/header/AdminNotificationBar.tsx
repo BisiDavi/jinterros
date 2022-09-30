d5a9de3a2589dff0ac4f1b4eaec3c1f1d2fc66ed
BisiDavi <oludavidconnect@gmail.com>
2022-09-30 05:33:04 +0100

    admin updated

--- components/header/AdminNotificationBar.tsx
+++ components/header/AdminNotificationBar.tsx
@@ -8,13 +8,13 @@ export default function AdminNotificationBar() {
   return (
     <>
       {authStatus && (
-        <div className="absolute right-5 top-5 flex items-center">
-          <div className="text flex flex-col items-end justify-items-end">
+        <div className="lg:absolute lg:right-5 lg:top-5 flex fixed bottom-20 lg:mt-0 items-center">
+          <div className="text flex order-2 lg:order-1 flex-col lg:items-end lg:justify-items-end">
             <h6 className="font-semibold">{authStatus?.displayName}</h6>
             <p className="font-medium">Admin</p>
           </div>
           {authStatus.displayName && (
-            <div className="profile-icon h-14 w-14 bg-gray-500 font-bold text-white flex items-center justify-center mx-4 rounded-full">
+            <div className="profile-icon order-1 lg:order-2 h-14 w-14 bg-gray-500 font-bold text-white flex items-center justify-center mx-4 rounded-full">
               {getUserInitials(authStatus?.displayName)}
             </div>
           )}
--- components/sidebar/MobileAdminSidebar.tsx
+++ components/sidebar/MobileAdminSidebar.tsx
@@ -6,6 +6,7 @@ import links from "@/json/links.json";
 import displayIcons from "@/lib/displayIcons";
 import Button from "@/components/button";
 import useAuthMutation from "@/hooks/useAuthMutation";
+import AdminNotificationBar from "../header/AdminNotificationBar";
 
 export default function MobileAdminSidebar() {
   const router = useRouter();
@@ -33,12 +34,7 @@ export default function MobileAdminSidebar() {
     );
   }
   return (
-    <aside className="sidebar hidden lg:flex h-screen flex flex-col relative w-1/5 border-r">
-      <div className="logo-view h-40 border-b flex flex-col">
-        <div className="w-2/5 flex mx-auto my-4">
-          <Logo link="/admin" />
-        </div>
-      </div>
+    <aside className="w-full h-screen fixed top-10 py-20 bg-white z-40">
       <ul className="link-group">
         {links.admin.map((item) => {
           const activeLink = getActiveLink(item.link);
@@ -57,10 +53,11 @@ export default function MobileAdminSidebar() {
           );
         })}
       </ul>
+      <AdminNotificationBar />
       <div className="wrapper absolute w-full bottom-14 flex items-center">
         <Button
           text="Logout"
-          className="bg-red-500 px-4 hover:opacity-70 rounded-lg bottom-0 text-white font-bold mx-auto justify-center py-1"
+          className="bg-red-500 px-4 hover:opacity-70 rounded-lg bottom-0 text-white font-bold ml-4 lg:ml-0 lg:mx-auto justify-center py-1"
           onClick={logout}
         />
       </div>
