16fef4a8fe4541592b796f52c25c1489f53ff939
BisiDavi <oludavidconnect@gmail.com>
2022-09-30 11:47:14 +0100

    admin page updated

--- pages/admin/settings/index.tsx
+++ pages/admin/settings/index.tsx
@@ -4,7 +4,7 @@ import AdminLayout from "@/layout/AdminLayout";
 export default function SettingsPage() {
   return (
     <AdminLayout title="Settings">
-      <section className="w-2/3 px-2">
+      <section className="lg:w-2/3 px-2">
         <h4 className="text-center font-bold text-xl mb-2">Create An Admin</h4>
         <AdminForm type="signup" />
       </section>
